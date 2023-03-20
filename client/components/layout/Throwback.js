1617d43ab5b10145d16f0e5cb40f80f6b3f3da1b
Pauloper1 <paulcrescent.p@gmail.com>
2023-03-20 18:12:09 +0000

    throwbackPhoto added

---
+++ client/components/layout/Throwback.js
@@ -0,0 +1,13 @@
+
+
+export default function Throwback(){
+    return(
+        <>
+        <throwback>
+        <div class="throwback">
+                    <h1>hello</h1>
+        </div>
+        </throwback>
+        </>
+    )
+}
